45261c63f624ab5cf6d90975727df06997c48a2a
PolloCraft <vidal2006de@gmail.com>
2026-08-21 13:27:54 -0500

    feat: add React Router and base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      <h1>Chamo Import</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<div><h1>Chamo Import</h1></div>} />
+      <Route path="/catalogo" element={<div><h1>Catálogo</h1></div>} />
+      <Route path="/producto/:id" element={<div><h1>Producto</h1></div>} />
+      <Route path="/contacto" element={<div><h1>Contacto</h1></div>} />
+      <Route path="*" element={<div><h1>404 - No encontrado</h1></div>} />
+    </Routes>
   )
 }
 
